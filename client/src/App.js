b123c2e6c7c2540056eb4e5a1c8e8d9faad6a15a
margoshkari <68592214+margoshkari@users.noreply.github.com>
2022-09-26 13:40:45 +0300

    upd

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,27 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  var turn = "X";
+  function Click(e) {
+    e.preventDefault();
+    if (e.target.innerHTML == "") {
+      turn = turn == "X" ? "0" : "X";
+      e.target.innerHTML = turn;
+    }
+  }
   return (
     <div className="App">
       <h2>Tic-Tac-Toe</h2>
       <div className="grid">
-        <div className="block"></div>
-        <div className="block"></div>
-        <div className="block"></div>
-        <div className="block"></div>
-        <div className="block"></div>
-        <div className="block"></div>
-        <div className="block"></div>
-        <div className="block"></div>
-        <div className="block"></div>
+        <div className="block" onClick={(e) => Click(e)}></div>
+        <div className="block" onClick={(e) => Click(e)}></div>
+        <div className="block" onClick={(e) => Click(e)}></div>
+        <div className="block" onClick={(e) => Click(e)}></div>
+        <div className="block" onClick={(e) => Click(e)}></div>
+        <div className="block" onClick={(e) => Click(e)}></div>
+        <div className="block" onClick={(e) => Click(e)}></div>
+        <div className="block" onClick={(e) => Click(e)}></div>
+        <div className="block" onClick={(e) => Click(e)}></div>
       </div>
     </div>
   );

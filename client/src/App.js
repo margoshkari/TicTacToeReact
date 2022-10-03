dd00941f4c74278c19223397f6715c221d778ddb
margoshkari <68592214+margoshkari@users.noreply.github.com>
2022-10-03 14:35:44 +0300

    added check function to backup

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,50 @@
 import "./App.css";
 
 function App() {
-  var turn = "X";
+  var isEnd = false;
+  var symb = "X";
+  var array = [];
+  array.length = 9;
   function Click(e) {
-    e.preventDefault();
-    if (e.target.innerHTML == "") {
-      turn = turn == "X" ? "0" : "X";
-      e.target.innerHTML = turn;
+    if (!isEnd) {
+      e.preventDefault();
+      if (e.target.innerHTML === "") {
+        symb = symb === "X" ? "0" : "X";
+        e.target.innerHTML = symb;
+        array[e.target.id] = e.target.innerHTML;
+        fetch("/api", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            blocks: array,
+          }),
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            if (data.answer != "") {
+              isEnd = true;
+              var winner = document.getElementById("winner");
+              winner.innerHTML = data.answer;
+              winner.style = "display: block;";
+            }
+          });
+      }
     }
   }
   return (
     <div className="App">
       <h2>Tic-Tac-Toe</h2>
+      <h2 id="winner"></h2>
       <div className="grid">
-        <div className="block" onClick={(e) => Click(e)}></div>
-        <div className="block" onClick={(e) => Click(e)}></div>
-        <div className="block" onClick={(e) => Click(e)}></div>
-        <div className="block" onClick={(e) => Click(e)}></div>
-        <div className="block" onClick={(e) => Click(e)}></div>
-        <div className="block" onClick={(e) => Click(e)}></div>
-        <div className="block" onClick={(e) => Click(e)}></div>
-        <div className="block" onClick={(e) => Click(e)}></div>
-        <div className="block" onClick={(e) => Click(e)}></div>
+        <div className="block" id="0" onClick={(e) => Click(e)}></div>
+        <div className="block" id="1" onClick={(e) => Click(e)}></div>
+        <div className="block" id="2" onClick={(e) => Click(e)}></div>
+        <div className="block" id="3" onClick={(e) => Click(e)}></div>
+        <div className="block" id="4" onClick={(e) => Click(e)}></div>
+        <div className="block" id="5" onClick={(e) => Click(e)}></div>
+        <div className="block" id="6" onClick={(e) => Click(e)}></div>
+        <div className="block" id="7" onClick={(e) => Click(e)}></div>
+        <div className="block" id="8" onClick={(e) => Click(e)}></div>
       </div>
     </div>
   );
--- server/app.js
+++ server/app.js
@@ -2,7 +2,60 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get(`/api`, async (req, res) => {});
+app.use(
+  express.json({
+    type: ["application/json", "text/plain"],
+  })
+);
+app.post(`/api`, async (req, res) => {
+  res.json({ answer: Check(req.body.blocks) });
+});
+function Check(field) {
+  for (let index = 0; index < field.length; index += 3) {
+    if (field[index] != null) {
+      if (
+        field[index] == field[index + 1] &&
+        field[index] == field[index + 2]
+      ) {
+        return Winner(field[index]);
+      }
+    }
+  }
+
+  for (let index = 0; index < 3; index++) {
+    if (field[index] != null) {
+      if (
+        field[index] == field[index + 3] &&
+        field[index] == field[index + 6]
+      ) {
+        return Winner(field[index]);
+      }
+    }
+  }
+  if (field[0] != null) {
+    if (field[0] == field[4] && field[0] == field[8]) {
+      return Winner(field[0]);
+    }
+  }
+  if (field[2] != null) {
+    if (field[2] == field[4] && field[2] == field[6]) {
+      return Winner(field[2]);
+    }
+  }
+  if (field.every((item) => item != null)) return Winner("");
+
+  return false;
+}
+
+function Winner(symb) {
+  if (symb == "X") {
+    return "Winner Cross!";
+  } else if (symb == "0") {
+    return "Winner Zero!";
+  } else {
+    return "Dead Heat!";
+  }
+}
 
 app.listen(port, () => {
   console.log(`Start server ${port}`);
